91f7769c5cc085c0c0cff1ac6174d1b15dfde28a
barronh <barronh@gmail.com>
2021-02-17 21:13:49 -0500

    Added spatial allocation
    Spatial allocation works. Functions to build regions from shapefiles
    are 99 percent of the way there. Tests with census shapefiles creates
    a weird double counting error between north and south carolina. Not sure
    if this is my method of the resolution of the data. Either way, with
    correctly built region files, the approach works fine.

---
+++ smokelite/allocators/spatial.py
@@ -0,0 +1,117 @@
+import PseudoNetCDF as pnc
+import pandas as pd
+
+
+class Spatial:
+    def __init__(
+        self, griddesc, gdnam, nominaldate='1970-01-01', **kwds
+    ):
+        nominaldate = pd.to_datetime(nominaldate)
+        gf = pnc.pncopen(griddesc, format='griddesc', GDNAM=gdnam)
+        gf.SDATE = int(nominaldate.strftime('%Y%j'))
+        gf.STIME = int(nominaldate.strftime('%H%M%S'))
+        gf.TSTEP = 10000
+        self.spatialfile = gf.subset([])
+        uv = self.spatialfile.createVariable(
+            'UNIFORM', 'f', ('TSTEP', 'LAY', 'ROW', 'COL'),
+            long_name='UNIFORM', var_desc='UNIFORM', units='none'
+        )
+        uv[:] = 1 / uv.size
+        self.regionfile = gf.subset([])
+        dw = self.regionfile.createVariable(
+            'DOMAINWIDE', 'f', ('TSTEP', 'LAY', 'ROW', 'COL'),
+            long_name='DOMAINWIDE', var_desc='DOMAINWIDE', units='fraction'
+        )
+        dw[:] = 1.
+        self.regions = ['DOMAINWIDE']
+
+    def region_fromshapefile(
+        self, shapepath, queryfield, query, key, fractional=True, verbose=0, simplify=None, buffer=0
+    ):
+        "Thin wrapper on fractional_overlap"
+        from ..util import fractional_overlap
+        return fractional_overlap(
+            ifile=self.regionfile, shapepath=shapepath, queryfield=queryfield,
+            query=query, key=key, fractional=fractional, verbose=verbose, simplify=simplify, buffer=buffer
+        )
+
+    def add_spatialvariables(self, spatialdf=None, spatialf=None, keys=None):
+        sptlf = self.spatialfile
+        if spatialf is not None and spatialdf is not None:
+            raise KeyError('supply either spatialdf or spatialvar; got both')
+        if spatialf is not None:
+            for key in keys:
+                sptlf.copyVariable(spatialf.variables[key], key=key)
+        if spatialdf is not None:
+            from ..util import load_dataframe
+            load_dataframe(sptlf, spatialdf)
+
+    def allocate(self, infile, alloc_keys, outpath=None, **save_kwds):
+        """
+        Arguments
+        ---------
+        infile : str or PseudoNetCDF File
+            path to netcdf file (or file) to use as input (format keyword used
+            as a modifier)
+        alloc_keys : mappable  or str
+            each key should exist in the vertical allocation file, and values
+            should correspond to variables in the infile. If is a str, then
+            all allocatable variables will be asisgned to that csv key.
+        outpath : str or None
+            path for output to be saved. If None, outf will be returned and not
+            saved
+
+        Returns
+        -------
+        outf : PseudoNetCDFFile
+            file with spatial variation
+
+        Notes
+        -----
+
+        """
+        if isinstance(infile, str):
+            infile = pnc.pncopen(infile, format=format)
+
+        if isinstance(alloc_keys, str):
+            alloc_keys = {alloc_keys: None}
+
+        all_keys = []
+        for k, v in infile.variables.items():
+            if 'LAY' in v.dimensions:
+                all_keys.append(k)
+
+        assigned_keys = []
+
+        isnone = []
+        for (region, srgkey), varkeys in alloc_keys.items():
+            if varkeys is None:
+                isnone.append((region, srgkey))
+            else:
+                assigned_keys.extend(varkeys)
+
+        unassigned_keys = list(set(all_keys).difference(assigned_keys))
+        if len(isnone) > 1:
+            raise ValueError(f'Can only have 1 None sector; got {isnone}')
+        if len(isnone) == 1:
+            alloc_keys[isnone[0]] = unassigned_keys
+
+        outf = self.spatialfile.subset([])
+        for (regionkey, allockey), varkeys in alloc_keys.items():
+            regionvar = self.regionfile.variables[regionkey]
+            allocvar = self.spatialfile.variables[allockey]
+            factor = regionvar[:] * allocvar[:]
+            factor /= factor.sum()
+            for varkey in varkeys:
+                invar = infile.variables[varkey]
+                outvar = outf.createVariable(
+                    varkey, 'f', ('TSTEP', 'LAY', 'ROW', 'COL'),
+                    long_name=varkeys, vardesc=varkey,
+                    units=getattr(invar, 'units', 'unknown')
+                )
+                outvar[:] = invar[:] * factor
+
+        if outpath is None:
+            return outf
+        else:
+            return outf.save(outpath, **save_kwds)
---
+++ smokelite/util/__init__.py
@@ -0,0 +1,206 @@
+__all__ = ['load_dataframe', 'fractional_overlap']
+
+
+def fractional_overlap(
+    ifile, shapepath, queryfield, query, key='MINE', fractional=True,
+    simplify=None, buffer=0, verbose=0
+):
+    """
+    Arguments
+    ---------
+    ifile : PseudoNetCDFFile ioapi_base
+        file that supports ij2ll, ll2xy, and xy2ll and has dimensions
+        ('TSTEP', 'LAY', 'ROW', 'COL')
+    shapepath : str
+        path to shapefile
+    queryfield : str
+        name of attribute to query for query
+    query : str or function
+        if str, any attribute with that value will be selected
+    key : str
+        key for output variable in ifile
+    fractional : bool
+        If True, then use fractional area overlap otherwise centroid
+    simplify : float or None
+        Simplify to speed-up processing.
+    buffer : float or None
+        If None, do not simplify. If float, buffer. This can be useful to
+        simplify self-intersecting polygons.
+
+    Returns
+    -------
+    mine : PseudoNetCDFVariable
+        variable with fractional area overlap
+    """
+    from types import FunctionType
+    import numpy as np
+    import shapefile as shp
+    from shapely.geometry import Polygon, Point
+    from shapely.prepared import prep
+    from shapely.ops import cascaded_union
+
+    shpf = shp.Reader(shapepath)
+    fldnames = [n for n, t, l, d in shpf.fields][1:]
+    if verbose > 0:
+        print('Field names', fldnames)
+    if verbose > 1:
+        print('Example fields', shpf.record(0))
+
+    if isinstance(query, FunctionType):
+        queryfunc = query
+    else:
+        def queryfunc(x):
+            return x == query
+
+    attridx = fldnames.index(queryfield)
+    if verbose > 0:
+        print('Query features...', flush=True, end='')
+
+    recordnums = [
+        ri for ri, rec in enumerate(shpf.iterRecords())
+        if queryfunc(rec[attridx])
+    ]
+    if verbose > 0:
+        print(f' found {len(recordnums)}')
+    if verbose == 2:
+        print([shpf.record(n)[attridx] for n in recordnums])
+    elif verbose > 2:
+        print(fldnames)
+        print([shpf.record(n) for n in recordnums])
+
+    # Centroids
+    I, J = np.meshgrid(np.arange(ifile.NCOLS), np.arange(ifile.NROWS))
+    LON, LAT = ifile.ij2ll(I, J)
+    if fractional:
+        # Centroids + 1
+        Id, Jd = np.meshgrid(
+            np.arange(ifile.NCOLS + 1),
+            np.arange(ifile.NROWS + 1)
+        )
+        LONd, LATd = ifile.ij2ll(Id, Jd)
+        # As X coordinates
+        Xd, Yd = ifile.ll2xy(LONd, LATd)
+        # As edge coordinates
+        XD = Xd - ifile.XCELL / 2
+        YD = Yd - ifile.YCELL / 2
+        LOND, LATD = ifile.xy2ll(XD, YD)
+
+    mine = ifile.createVariable(
+        key, 'f', ('TSTEP', 'LAY', 'ROW', 'COL'),
+        units='fraction', long_name=key, var_desc=key
+    )
+    mine.var_desc = '0 means not mine, 1 means all mine, inbetween is partial'
+    if verbose > 0:
+        print('Making polygons...', flush=True)
+    shapes = [shpf.shape(rn) for rn in recordnums]
+    polygons = [Polygon(s.points) for s in shapes]
+    if buffer is not None:
+        if verbose > 0:
+            print(f'Buffer shapes with buffer {buffer}...', flush=True)
+        polygons = [p.buffer(buffer) for p in polygons]
+    if simplify is not None:
+        if verbose > 0:
+            print(f'Simplifying shapes with simplify {simplify}...', flush=True)
+        polygons = [p.simplify(simplify) for p in polygons]
+    # ppolygons = [prep(p) for p in polygons]
+    if len(polygons) > 1:
+        if verbose > 0:
+            print(f'Cascading union...', flush=True)
+        uberpoly = cascaded_union(polygons)
+    else:
+        uberpoly = polygons[0]
+    if verbose > 0:
+        print('Making envelope...', flush=True)
+    envelope = uberpoly.envelope
+    if verbose > 0:
+        print('Making prepared uber polygon...', flush=True)
+    puberpoly = prep(uberpoly)
+    if verbose:
+        onepercent = int(ifile.NROWS * ifile.NCOLS / 100)
+        print('Processing cells', flush=True)
+    for j, i in np.ndindex(ifile.NROWS, ifile.NCOLS):
+        if verbose > 0:
+            n = j * ifile.NCOLS + i
+            if n % onepercent == 0:
+                print('.', end='', flush=True)
+        if fractional:
+            gpoly = Polygon([
+                [LOND[j + 0, i + 0], LATD[j + 0, i + 0]],
+                [LOND[j + 0, i + 1], LATD[j + 0, i + 1]],
+                [LOND[j + 1, i + 1], LATD[j + 1, i + 1]],
+                [LOND[j + 1, i + 0], LATD[j + 1, i + 0]],
+                [LOND[j + 0, i + 0], LATD[j + 0, i + 0]],
+            ])
+            if envelope.intersects(gpoly):
+                if puberpoly.intersects(gpoly):
+                    intx = gpoly.intersection(uberpoly)
+                    farea = intx.area / gpoly.area
+                    mine[0, 0, j, i] = farea
+        else:
+            gp = Point(LON[j, i], LAT[j, i])
+            if envelope.intersects(gpoly):
+                if puberpoly.contains(gp):
+                    if uberpoly.contains(gp):
+                        mine[0, 0, j, i] = 1
+    if verbose > 0:
+        print()
+    return mine
+
+
+def load_dataframe(
+    pfile, df, func='mean', units='unknown',
+    dims=None, coords=False
+):
+    """
+    Arguments
+    ---------
+    pfile : PseudoNetCDFFile
+        must have dimensions ('TSTEP', 'LAY', 'ROW', 'COL')
+    df : pandas.DataFrame
+        must have columns matching dimensions
+    func : str
+        Used to aggregate values within a unique coordinate
+    units : str
+        Default value for units
+    dims : tuple or None
+        If None, defaults to ('TSTEP', 'LAY', 'ROW', 'COL'), otherwise
+        tuple of strings indicating dimensions for new variables
+    coords : bool
+        If True, copy dimension variables as IN_{dim}
+    Returns
+    -------
+    pfile : PseudoNetCDFFile
+    """
+    incols = df.columns
+    # dims = pfile.dimensions
+    dims = ('TSTEP', 'LAY', 'ROW', 'COL')
+    gkeys = [k for k in incols if k in pfile.dimensions]
+    dfgb = df.groupby(gkeys, as_index=False)
+    gdf = getattr(dfgb, func)()
+    vals = {k: gdf[k].values for k in gkeys}
+    slices = {}
+    for k, v in vals.items():
+        if k in pfile.variables:
+            slices[k] = pfile.val2idx(k, v)
+        else:
+            slices[k] = v.astype('i')
+
+    slicer = tuple([
+        slices.get(k, slice(None))
+        for k in dims
+    ])
+
+    for outkey in incols:
+        outval = gdf[outkey]
+        if outkey in gkeys:
+            if not coords:
+                continue
+
+            outkey = f'IN_{outkey}'
+        var = pfile.createVariable(
+            outkey, outval.dtype.char, dims,
+            units=units, long_name=outkey.ljust(16), var_desc=outkey.ljust(80)
+        )
+        var[slicer] = outval
+
+    return pfile
